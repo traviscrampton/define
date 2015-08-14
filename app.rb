1d93fc64c5138339fb0619516ef3378858ef06f5
Travis Crampton <crampton.travis@gmail.com>
2015-08-14 10:36:04 -0700

    add content to app.rb

--- app.rb
+++ app.rb
@@ -7,3 +7,27 @@ require('./lib/word')
 get('/') do
   erb(:index)
 end
+
+get('/words/new') do
+  erb(:words_form)
+end
+
+get('/words') do
+  @dealerships = Word.all()
+  erb(:words)
+end
+
+post('/words') do
+  name = params.fetch('name')
+  Word.new(name).save()
+  @words = Word.all()
+  erb(:success)
+end
+
+get('/definitions/:id') do
+  @vehicle = Definition.find(params.fetch('id').to_i())
+  @make = @vehicle.make()
+  @model = @vehicle.model()
+  @year = @vehicle.year()
+  erb(:definition)
+end
---
+++ spec/define_integration_spec.rb
@@ -0,0 +1,13 @@
+# require('capybara/rspec')
+# require('./app')
+# Capybara.app = Sinatra::Application
+# set(:show_exceptions, false)
+#
+# describe('phonebook path', {:type => :feature}) do
+#   it('processes the user entry and adds a new phonebook') do
+#     visit('/phonebooks/new')
+#     fill_in('name', :with => 'Travis')
+#     click_button('Add')
+#     expect(page).to have_content('Success!')
+#   end
+# end
